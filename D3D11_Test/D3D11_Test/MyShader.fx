2264b4ff84d65dc18b8cc591f555c48ea5c316dd
Sorae <seokmin@nhnnext.org>
2016-05-08 20:45:03 +0900

    텍스쳐

--- D3D11_Test/D3D11_Test/MyShader.fx
+++ D3D11_Test/D3D11_Test/MyShader.fx
@@ -1,16 +1,35 @@
+Texture2D texDiffuse;
+SamplerState samLinear;
+
 struct VertexIn
 {
 	float3 PosL  : POSITION;
 	float4 Color : COLOR;
+	
+	float3 normal: NORMAL;
+
+	float2 tex : TEXCOORD;
 };
 
 struct VertexOut
 {
 	float4 PosH  : SV_POSITION;
 	float4 Color : COLOR;
+
+	float4 normal : NORMAL;
+
+	float2 tex : TEXCOORD;
 };
 
-VertexOut VS(VertexIn vin)
+cbuffer ConstantBuffer
+{
+	float4x4 wvp;
+	float4x4 world;
+	
+	float4 lightDir;
+	float4 lightColor;
+};
+/*VertexOut VS(VertexIn vin)
 {
 	VertexOut vout;
 
@@ -21,9 +40,40 @@ VertexOut VS(VertexIn vin)
 	vout.Color = vin.Color;
 
 	return vout;
+}*/
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout;
+
+	vout.PosH = mul(float4(vin.PosL, 1.f), wvp);
+	vout.Color = vin.Color;
+
+	vout.normal = mul(float4(vin.normal, 0.f), world);
+
+	vout.tex = vin.tex;
+
+	return vout;
 }
 
 float4 PS(VertexOut pin):SV_TARGET
 {
-	return pin.Color;
+	float4 finalColor = 0;
+
+	finalColor = saturate((dot((float3) - lightDir, pin.normal)*0.5+0.5)*lightColor);
+	
+	float4 texColor = texDiffuse.Sample(samLinear, pin.tex) * finalColor;
+	texColor.a = 1.f;
+
+	return texColor;
+}
+
+technique11 ColorTech
+{
+	pass P0
+	{
+		SetVertexShader(CompileShader(vs_5_0, VS()));
+		SetGeometryShader(NULL);
+		SetPixelShader(CompileShader(ps_5_0, PS()));
+	}
 }
--- D3D11_Test/D3D11_Test/WinMain.cpp
+++ D3D11_Test/D3D11_Test/WinMain.cpp
@@ -4,9 +4,9 @@
 #include <d3d11.h>
 #include <d3dCompiler.h>
 #include <d3dx11.h>
-#include <dxerr.h>
-
+#include "dxerr.h"
 #include "d3dUtil.h"
+#include "MyTime.h"
 //버퍼용 구조체
 
 template<class T, size_t N>
@@ -15,8 +15,20 @@ size_t size(T(&)[N]) { return N; }
 struct MyVertex {
 	XMFLOAT3 pos;
 	XMFLOAT4 color;
-};
 
+	XMFLOAT3 normal;
+
+	XMFLOAT2 tex;
+};
+struct	 ConstantBuffer
+{
+	XMMATRIX wvp;
+	XMMATRIX world;
+	XMFLOAT4 lightDir;
+	XMFLOAT4 lightColor;
+};
+XMFLOAT4 lightDirection = { 1.f,1.f,1.f,1.f };
+XMFLOAT4 lightColor = { 1.f,1.f,1.f,1.f };
 #define szWindowClass	TEXT("First")
 #define szTitle			TEXT("First App")
 
@@ -36,8 +48,78 @@ ID3D11Buffer*			g_pIndexBuffer = nullptr;
 ID3D11PixelShader*		g_pPixelShader = nullptr;//픽셀쉐이더
 DWORD					g_nIndex = 0;
 
+
+
+XMMATRIX                g_World;
+XMMATRIX                g_World2;
+XMMATRIX                g_View;
+XMMATRIX                g_Projection;
+ID3D11Buffer*			g_pConstantBuffer = nullptr;
+
+ID3D11Texture2D*			g_pDepthStencil = nullptr;
+ID3D11DepthStencilView*		g_pDepthStencilView = nullptr;
+
+ID3D11ShaderResourceView*	g_pTextureRV = nullptr;
+ID3D11SamplerState*			g_pSamplerLinear = nullptr;
+
 #define FAIL_CHECK(_hr_)\
 if(FAILED(_hr_)) return _hr_
+
+ID3D11RasterizerState *		g_pSolidRS;
+ID3D11RasterizerState *		g_pWireFrameRS;
+
+void CreateRenderState1()
+{
+	D3D11_RASTERIZER_DESC      rasterizerDesc;
+	ZeroMemory(&rasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+	rasterizerDesc.FillMode = D3D11_FILL_SOLID;		// Fill 옵션
+	rasterizerDesc.CullMode = D3D11_CULL_BACK;	// Culling 옵션
+	rasterizerDesc.FrontCounterClockwise = false;		// 앞/뒷면 로직 선택
+
+	g_pd3dDevice->CreateRasterizerState(&rasterizerDesc, &g_pSolidRS);
+}
+void CreateRenderState2()
+{
+	D3D11_RASTERIZER_DESC      rasterizerDesc;
+	ZeroMemory(&rasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+	rasterizerDesc.FillMode = D3D11_FILL_WIREFRAME;		// Fill 옵션
+	rasterizerDesc.CullMode = D3D11_CULL_NONE;	// Culling 옵션
+	rasterizerDesc.FrontCounterClockwise = false;		// 앞/뒷면 로직 선택
+
+	g_pd3dDevice->CreateRasterizerState(&rasterizerDesc, &g_pWireFrameRS);
+}
+
+HRESULT   LoadTexture()
+{
+	
+	HRESULT hr = D3DX11CreateShaderResourceViewFromFile(
+		g_pd3dDevice,
+		L"Textures/images.jpg",
+		nullptr,
+		nullptr,
+		&g_pTextureRV,
+		nullptr
+	);
+	FAIL_CHECK(hr);
+	D3D11_SAMPLER_DESC sampDesc;
+	ZeroMemory(&sampDesc, sizeof(sampDesc));
+	sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.MinLOD = 0;
+	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
+
+	hr = g_pd3dDevice->CreateSamplerState(&sampDesc, &g_pSamplerLinear);
+	FAIL_CHECK(hr);
+
+	return S_OK;
+}
+
+void CalculateMatrixForBox(float dt);
+void CalculateMatrixForBox2(float dt);
+void CreateDepthStencilTexture();
 
 HRESULT InitDevice()
 {
@@ -105,9 +187,10 @@ HRESULT InitDevice()
 	pBackBuffer->Release();//get 해온 뒤에는 꼭 release
 	FAIL_CHECK(hr);
 
+	CreateDepthStencilTexture();
 	g_pImmediateContext->OMSetRenderTargets(1,//렌더 대상 갯수. 장면 분할시 1 초과
 		&g_pRenderTargetView,
-		nullptr);//깊이 / 스텐실 버퍼
+		g_pDepthStencilView);//깊이 / 스텐실 버퍼
 
 
 	D3D11_VIEWPORT vp;
@@ -122,12 +205,14 @@ HRESULT InitDevice()
 	return hr;
 }
 
-void Render()
+void Render(float dt)
 {
 	//just clear the backbuffer
 	float ClearColor[4] = { 0.3f,0.3f,0.3f,1.f };//r,g,b,a
 	//clear
 	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+
+	g_pImmediateContext->ClearDepthStencilView(g_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.f, 0);
 
 	//render code
 	{
@@ -142,7 +227,18 @@ void Render()
 		//set shader and draw
 		g_pImmediateContext->VSSetShader(g_pVertexShader, nullptr, 0);
 		g_pImmediateContext->PSSetShader(g_pPixelShader, nullptr, 0);
-		g_pImmediateContext->DrawIndexed(6, 0, 0);
+
+		//texture settings
+		g_pImmediateContext->PSSetShaderResources(0, 1, &g_pTextureRV);
+		g_pImmediateContext->PSSetSamplers(0, 1, &g_pSamplerLinear);
+
+		g_pImmediateContext->RSSetState(g_pSolidRS);
+		CalculateMatrixForBox(dt);
+		g_pImmediateContext->DrawIndexed(36, 0, 0);
+		
+		//g_pImmediateContext->RSSetState(g_pWireFrameRS);
+		CalculateMatrixForBox2(dt);
+		g_pImmediateContext->DrawIndexed(36, 0, 0);
 	}
 
 
@@ -158,7 +254,12 @@ void CleanupDevice()
 	CHECK_AND_RELEASE(g_pVertexLayout);
 	CHECK_AND_RELEASE(g_pVertexShader);
 	CHECK_AND_RELEASE(g_pPixelShader);
-
+	CHECK_AND_RELEASE(g_pConstantBuffer);
+	CHECK_AND_RELEASE(g_pDepthStencilView);
+	CHECK_AND_RELEASE(g_pDepthStencil);
+	CHECK_AND_RELEASE(g_pSolidRS);
+	CHECK_AND_RELEASE(g_pSamplerLinear);
+	CHECK_AND_RELEASE(g_pTextureRV);
 	if (g_pImmediateContext)
 		g_pImmediateContext->ClearState();
 	if (g_pRenderTargetView)
@@ -168,6 +269,35 @@ void CleanupDevice()
 	if (g_pd3dDevice)
 		g_pd3dDevice->Release();
 }
+
+void CreateDepthStencilTexture()
+{
+	//create depth stencil texture
+	D3D11_TEXTURE2D_DESC descDepth;
+	ZeroMemory(&descDepth, sizeof(descDepth));
+	descDepth.Width = 800;
+	descDepth.Height = 600;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+	g_pd3dDevice->CreateTexture2D(&descDepth, nullptr, &g_pDepthStencil);
+
+	//create depth stencil view
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+	ZeroMemory(&descDSV, sizeof(descDSV));
+	descDSV.Format = descDepth.Format;
+	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDSV.Texture2D.MipSlice = 0;
+	descDSV.Flags = 0;
+	g_pd3dDevice->CreateDepthStencilView(g_pDepthStencil, &descDSV, &g_pDepthStencilView);
+}
+
 
 HRESULT CreateShader()
 {
@@ -193,7 +323,9 @@ HRESULT CreateShader()
 	D3D11_INPUT_ELEMENT_DESC	layout[] =
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+		{ "COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT,D3D11_INPUT_PER_VERTEX_DATA,0}
 	};//D3D!!_INPUT_ELEMENT_DESC : Semantic, Semantic Index, 자료형태, 사용할 슬롯(0~15), AlignedByteOffset(D3D11_APPEND_ALIGNED_ELEMENT 사용시 자동 계산,입력 분류(정점당,인스턴스당), 성분별 반복 렌더 개수
 	UINT numElements = ARRAYSIZE(layout);
 	hr = g_pd3dDevice->CreateInputLayout(layout, numElements,//성분
@@ -222,18 +354,47 @@ HRESULT CreateShader()
 //정점 버퍼 생성
 HRESULT CreateVertexBufferAndIndexBuffer()
 {
-	MyVertex vertices[] = 
+// 	MyVertex vertices[] = 
+// 	{
+// 		{XMFLOAT3(0.7f,0.7f,1.f),(const float*)&Colors::Red},	//0
+// 		{XMFLOAT3(0.7f,-0.7f,1.f),(const float*)&Colors::Blue },	//1
+// 		{XMFLOAT3(-0.7f,-0.7f,1.f),(const float*)&Colors::Green },//2
+// 		{ XMFLOAT3(-0.7f,0.7f,1.f),(const float*)&Colors::White },//3
+// 	};
+// 	UINT indices[] =
+// 	{
+// 		0,1,2,
+// 		2,3,0
+// 	};
+	MyVertex vertices[] =
 	{
-		{XMFLOAT3(0.7f,0.7f,1.f),(const float*)&Colors::Red},	//0
-		{XMFLOAT3(0.7f,-0.7f,1.f),(const float*)&Colors::Blue },	//1
-		{XMFLOAT3(-0.7f,-0.7f,1.f),(const float*)&Colors::Green },//2
-		{ XMFLOAT3(-0.7f,0.7f,1.f),(const float*)&Colors::White },//3
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f),  XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f), XMFLOAT3(-0.33f, 0.33f, -0.33f) , XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f),   XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f), XMFLOAT3(0.33f, 0.33f, -0.33f) , XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f),    XMFLOAT4(0.0f, 1.0f, 1.0f, 1.0f), XMFLOAT3(0.33f, 0.33f, 0.33f) , XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f),   XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f), XMFLOAT3(-0.33f, 0.33f, 0.33f) , XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f), XMFLOAT3(-0.33f, -0.33f, -0.33f) , XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f),  XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f), XMFLOAT3(0.33f, -0.33f, -0.33f) , XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f),   XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f), XMFLOAT3(0.33f, -0.33f, 0.33f) , XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f),  XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f), XMFLOAT3(-0.33f, -0.33f, 0.33f) , XMFLOAT2(0.0f, 1.0f) },
 	};
+
+
 	UINT indices[] =
 	{
-		0,1,2,
-		2,3,0
+		3, 1, 0,
+		2, 1, 3,
+		0, 5, 4,
+		1, 5, 0,
+		3, 4, 7,
+		0, 4, 3,
+		1, 6, 5,
+		2, 6, 1,
+		2, 7, 6,
+		3, 7, 2,
+		6, 4, 5,
+		7, 4, 6,
 	};
+
 	g_nIndex = size(indices);
 
 	D3D11_BUFFER_DESC bd;
@@ -265,6 +426,77 @@ HRESULT CreateVertexBufferAndIndexBuffer()
 		&initData,
 		&g_pIndexBuffer);
 	return hr;
+}
+
+HRESULT CreateConstantBuffer()
+{
+	D3D11_BUFFER_DESC cbd;
+	ZeroMemory(&cbd, sizeof(cbd));
+	cbd.Usage = D3D11_USAGE_DEFAULT;
+	cbd.ByteWidth = sizeof(ConstantBuffer);
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.CPUAccessFlags = 0;
+	g_pd3dDevice->CreateBuffer(&cbd, nullptr, &g_pConstantBuffer);
+
+	return S_OK;
+}
+
+void CalculateMatrixForBox(float dt)
+{
+	XMMATRIX mat = XMMatrixRotationY(dt);
+	mat *= XMMatrixRotationX(-dt);
+	g_World = mat;
+
+	XMMATRIX wvp = g_World * g_View * g_Projection;
+	ConstantBuffer cb;
+	cb.wvp = XMMatrixTranspose(wvp);
+
+	cb.world = XMMatrixTranspose(g_World);
+	cb.lightDir = lightDirection;
+	cb.lightColor = lightColor;
+
+	g_pImmediateContext->UpdateSubresource(g_pConstantBuffer, 0, 0, &cb, 0, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+	g_pImmediateContext->PSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+}
+void CalculateMatrixForBox2(float dt)
+{
+	float scaleValue = sinf(dt)*0.5f + 1;
+	XMMATRIX scale = XMMatrixScaling(scaleValue, scaleValue, scaleValue); // scale
+	XMMATRIX rotate = XMMatrixRotationZ(dt);	// rotate
+	float moveValue = cosf(dt) * 5.f;	// move position
+	XMMATRIX position = XMMatrixTranslation(moveValue, 0.0f, 0.0f);
+	g_World2 = scale * rotate * position;	// S * R * T
+
+	XMMATRIX wvp = g_World2 * g_View * g_Projection;
+	ConstantBuffer cb;
+	cb.wvp = XMMatrixTranspose(wvp);
+
+	cb.world = XMMatrixTranspose(g_World);
+	cb.lightDir = lightDirection;
+	cb.lightColor = lightColor;
+
+	g_pImmediateContext->UpdateSubresource(g_pConstantBuffer, 0, 0, &cb, 0, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+	g_pImmediateContext->PSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+}
+
+HRESULT InitMatrix()
+{
+	
+
+	g_World = XMMatrixIdentity();
+
+	XMVECTOR pos = XMVectorSet(0.f, 0.f, -5.f, 1.f);
+	XMVECTOR target = XMVectorSet(0.f, 0.f, 1.f, 0.f);
+	XMVECTOR up = XMVectorSet(0.f, 1.f, 0.f, 0.f);
+	g_View = XMMatrixLookAtLH(pos, target, up);
+
+	g_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV2,
+		800.f / (FLOAT)600.f,
+		0.01f, 100.f);
+	return S_OK;
+
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd
@@ -319,9 +551,19 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	CreateShader();
 	CreateVertexBufferAndIndexBuffer();
 
+	CreateConstantBuffer();
+
+	CreateRenderState1();
+	CreateRenderState2();
+
+	LoadTexture();
+	InitMatrix();
+
+	CMyTime::GetInstance()->Init();
 	MSG			msg;
 	while (true)
 	{
+		CMyTime::GetInstance()->ProcessTime();
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			if (msg.message == WM_QUIT)
@@ -332,7 +574,10 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 		}
 		else
 		{
-			Render();
+			static float deltaTime = 0;
+			deltaTime += CMyTime::GetInstance()->GetElapsedTime();		// GetDeltaTime이 없으면 0.00005f등의 작은수를 쓸 것.
+
+			Render(deltaTime);
 		}
 	}
 	CleanupDevice();
